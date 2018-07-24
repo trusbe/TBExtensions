8221edb71ec05d0434c5620cc35cc10ff0c2bf69
TrusBe <theplough.891127@163.com>
2018-07-24 23:30:14 +0800

    + add int to data; + add data to int ...

--- TBExtensions/TBExtensions/Foundation/TBInt.swift
+++ TBExtensions/TBExtensions/Foundation/TBInt.swift
@@ -19,4 +19,39 @@ extension Int {
     var tb_toHex: String {
         return String(self, radix: 16)
     }
+    
+    var data: Data {
+        var int = self
+        return Data(bytes: &int, count: MemoryLayout<Int>.size)
+    }
+}
+
+extension UInt8 {
+    var data: Data {
+        var int = self
+        return Data(bytes: &int, count: MemoryLayout<UInt8>.size)
+    }
+}
+
+extension UInt16 {
+    var data: Data {
+        var int = self
+        return Data(bytes: &int, count: MemoryLayout<UInt16>.size)
+    }
+}
+
+extension UInt32 {
+    var data: Data {
+        var int = self
+        return Data(bytes: &int, count: MemoryLayout<UInt32>.size)
+    }
+    
+    var byteArrayLittleEndian: [UInt8] {
+        return [
+            UInt8((self & 0xFF000000) >> 24),
+            UInt8((self & 0x00FF0000) >> 16),
+            UInt8((self & 0x0000FF00) >> 8),
+            UInt8(self & 0x000000FF)
+        ]
+    }
 }
